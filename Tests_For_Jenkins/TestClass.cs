b8255dc2a4273d8943162f77f414f946576a367a
Anton <tottiasroma1991@gmail.com>
2021-07-30 19:22:07 +0300

    first commit

---
+++ Tests_For_Jenkins/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tests_For_Jenkins
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://habr.com/");
+
+            Assert.IsTrue(driver.Url.Contains("habr.com"), "Что-то не так =(");
+
+            driver.Quit();
+        }
+    }
+}
