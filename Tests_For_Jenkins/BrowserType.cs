e7e276d01fa31e35f5d9c9fc5f60bb622abd8f85
AntonAndreev22088 <anton.andreev@itechcraft.com>
2021-08-12 20:04:58 +0300

    commit all files

---
+++ Tests_For_Jenkins/BrowserType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests_For_Jenkins
+{
+    public enum BrowserType
+    {
+        Chrome,
+        Firefox
+    }
+}
---
+++ Tests_For_Jenkins/Hooks.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace Tests_For_Jenkins
+{
+    public class Hooks : BaseTest
+    {
+        private BrowserType _browserType;
+
+        [SetUp]
+        public void InitializeTest()
+        {
+            var browserType = TestContext.Parameters.Get("Browser", "Firefox");
+
+            _browserType = (BrowserType)Enum.Parse(typeof(BrowserType), browserType);
+
+            ChooseDriverInstance(_browserType);
+        }
+
+        private void ChooseDriverInstance(BrowserType browserType)
+        {
+            if (browserType == BrowserType.Chrome)
+            {
+                Driver = new ChromeDriver();
+            }
+            else 
+            {
+                FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
+                service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+                service.HideCommandPromptWindow = true;
+                service.SuppressInitialDiagnosticInformation = true;
+                Driver = new FirefoxDriver(service);
+            }
+        }
+
+        [TearDown]
+        public void TearDown() 
+        {
+            Driver.Quit();
+        }
+    }
+}
