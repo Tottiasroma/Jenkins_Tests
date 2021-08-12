7659cdd1ca9b532d948db8d81ab5e632976f5e86
AntonAndreev22088 <anton.andreev@itechcraft.com>
2021-08-12 19:57:59 +0300

    Add Firefox tests

--- Tests_For_Jenkins/TestClass.cs
+++ Tests_For_Jenkins/TestClass.cs
@@ -1,25 +1,19 @@
 ﻿
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using Assert = NUnit.Framework.Assert;
 
 namespace Tests_For_Jenkins
 {
-    public class TestClass
+    public class TestClass : Hooks
     {
         [Test]
         public void Test1()
         {
             //Open Habr Page
 
-            IWebDriver driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("http://habr.com/");
 
-            driver.Navigate().GoToUrl("http://habr.com/");
-
-            Assert.IsTrue(driver.Url.Contains("habr.com"), "Что-то не так =(");
-
-            driver.Quit();
+            Assert.IsTrue(Driver.Url.Contains("habr.com"), "Что-то не так =(");
         }
     }
 }
